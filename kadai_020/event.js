7846b47f926a80d75f574a66eef5cc66d88fae25
TaniguchiRio <ice24212421@gmail.com>
2025-05-22 13:35:53 +0900

    Update event.js
    textも定数に代入しました

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,10 @@
-// textというidを持つHTML要素を取得し、定数に代入する
+// btnというidを持つHTML要素を取得し、定数に代入する
 const page = document.getElementById('btn');
+// textというidを持つHTML要素を取得し、定数に代入する
+const newText = document.getElementById('text');
+
 // HTML要素がクリックされたときにイベント処理を実行する
 page.addEventListener('click', () => {
   console.log('ボタンをクリックしました');
-  text.textContent = 'ボタンをクリックしました';
+  newText.textContent = 'ボタンをクリックしました';
 });
