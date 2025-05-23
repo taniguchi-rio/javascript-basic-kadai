4b3319cc4c92bb14cf5af00ee2aac336e61a3d40
TaniguchiRio <ice24212421@gmail.com>
2025-05-23 13:30:02 +0900

    kadai_021
    非同期処理

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    console.log('ボタンをクリックしました');
+    text.textContent = 'ボタンをクリックしました';
+  }, 2000);
+  });
